b1daa3006323ea2468bf0eee3eb4ca9821e70ddf
Eirini Graonidou <egraonidou@gmail.com>
2020-12-27 18:41:48 +0100

    finished project

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {FETCH_USER} from './type';
+import {FETCH_SURVEYS} from './type';
 
 
 export const fetchUser = () => async dispatch => {
@@ -7,12 +8,20 @@ export const fetchUser = () => async dispatch => {
     dispatch({ type: FETCH_USER, payload: res.data});
 };
 
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+    dispatch({ type: FETCH_SURVEYS, payload: res.data});
+};
+
+
 export const handleToken = (token) => async dispatch => {
     const res = await axios.post('/api/stripe', token);
     dispatch({ type: FETCH_USER, payload: res.data});
 };
 
-export const submitSurvey = (values) => async dispatch => {
+export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post('/api/surveys', values);
+
+    history.push('/surveys');
     dispatch({type: FETCH_USER, playload: res.data});
 };
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,9 +2,10 @@ import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
-const SurveyReview = ({ onCancel, formValues, submitSurvey}) => {
+const SurveyReview = ({ onCancel, formValues, submitSurvey, history}) => {
     const reviewFields = _.map(formFields, ({name, label}) => {
         return (
             <div key={name}>
@@ -23,7 +24,7 @@ const SurveyReview = ({ onCancel, formValues, submitSurvey}) => {
                     onClick={onCancel}>Back
             </button>
             <button className="green darken-1 btn-flat right white-text"
-                    onClick={() => submitSurvey(formValues)}>Send Survey
+                    onClick={() => submitSurvey(formValues, history)}>Send Survey
                     <i className="material-icons right">email</i>
             </button>
         </div>
@@ -34,4 +35,4 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyReview));
